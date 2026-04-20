559b0228236156a9d013b6fc224ca2a967b160dd
Nayeli Guayas <guayasnayeli2003@gmail.com>
2026-04-19 22:52:31 -0500

    wip: working on first request

--- src/main.py
+++ src/main.py
@@ -1,4 +1,23 @@
-from config.settings import TARGET_USER, LIMIT
+from session.session_manager import create_session
+from config.settings import TARGET_USER
 
-print("Usuario:", TARGET_USER)
-print("Límite:", LIMIT)
+def main():
+    session = create_session()
+
+    url = f"https://i.instagram.com/api/v1/users/web_profile_info/?username={TARGET_USER}"
+
+    response = session.get(url)
+
+    print("Status:", response.status_code)
+
+    try:
+        data = response.json()
+        print("Respuesta recibida correctamente")
+        print(data)
+    except:
+        print("Error al obtener JSON")
+        print(response.text)
+
+
+if __name__ == "__main__":
+    main()
